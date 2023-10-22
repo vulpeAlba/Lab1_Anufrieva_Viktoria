d83302a62c2be1e31ab1b96860d5def76df7e125
Törrie <90221448+vulpeAlba@users.noreply.github.com>
2023-10-22 20:51:17 +0300

    Lab2

---
+++ lab2/task2.py
@@ -0,0 +1,14 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+month = 0
+
+while True:
+    money_capital += salary - spend
+    spend += spend * increase
+    if money_capital < 0:
+        break
+    month += 1
+
+print("Количество месяцев, которое можно протянуть без долгов:", month)
