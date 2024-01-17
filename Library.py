3e9e6261bd39806d07dc241821a8608e4aa3070f
Törrie <90221448+vulpeAlba@users.noreply.github.com>
2024-01-17 23:59:04 +0300

    Update Library.py
    Library.py is fixed

--- Library.py
+++ Library.py
@@ -5,7 +5,7 @@ class Library:
         self.books = books  # Список книг
 
     def get_next_book_id(self):
-        if len(self.books) == 0:
+        if not self.books:
             return 1
         else:
             last_book = self.books[-1]
